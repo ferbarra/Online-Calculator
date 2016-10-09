a1d04b8791589a59c982c7d0a2e7c47b1aec16b7
Fernando Barra <fernandobarra2207@gmail.com>
2016-10-09 01:17:50 -0400

    adds jQuery

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    let screen = '';
+    let buttons = document.querySelectorAll("button");
+    for (let i = 0, length = buttons.length; i < length; i++) {
+        buttons[i].addEventListener('click', calculate(event, this));
+    }
+    function calculate(a) {
+        let button = a;
+        let value = button.value;
+        console.log(value);
+    }
 });
